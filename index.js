46f62b3769c17da2cbe89150b0eed6ff87efd5c1
Deepika Govindaswamy <deepikagovindaswamy@gmail.com>
2026-05-19 11:42:31 +0100

    Modify log and output message in node app

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node-Express demo!')
-})
+  res.send('Hello World from Node Demo App! :)')
+});
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Node app running on ${port}`)
+});
