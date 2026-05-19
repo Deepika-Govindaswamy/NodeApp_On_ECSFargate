bb492afd4c4b933d9996838618d6499aba9c5864
Deepika Govindaswamy <deepikagovindaswamy@gmail.com>
2026-05-19 13:26:34 +0100

    Change hello message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node Demo App! :)')
+  res.send('Hello World - Node Demo App! :)')
 });
 
 app.listen(port, () => {
